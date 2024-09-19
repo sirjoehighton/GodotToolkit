2f6490d2b4024213dab21b118b458a50d36025c2
SirJoe <me@joehighton.com>
2024-09-19 16:03:04 +0100

    Removed 2 Functions that are already supported offically

--- addons/godottoolkit/toolkit.gd
+++ addons/godottoolkit/toolkit.gd
@@ -1,11 +1,5 @@
 extends Node
 
-func array_to_continuous_string(INPUT : Array) -> String:
-	var s = ""
-	for i in INPUT:
-		s += str(i)+" "
-	return s
-
 func string_to_bool(INPUT : String) -> bool:
 	var str = INPUT.strip_edges().to_lower()
 	if str in ["true", "1", "yes"]:
@@ -14,46 +8,3 @@ func string_to_bool(INPUT : String) -> bool:
 		return false
 	else:
 		return false
-
-func type_to_string(TYPE : int) -> String:
-	match TYPE:
-		TYPE_NIL: return "Null"
-		TYPE_BOOL: return "Boolean"
-		TYPE_INT: return "Integer"
-		TYPE_FLOAT: return "Float"
-		TYPE_STRING: return "String"
-		TYPE_VECTOR2: return "Vector2"
-		TYPE_VECTOR2I: return "Vector2i"
-		TYPE_RECT2: return "Rect2"
-		TYPE_RECT2I: return "Rect2i"
-		TYPE_VECTOR3: return "Vector3"
-		TYPE_VECTOR3I: return "Vector3i"
-		TYPE_TRANSFORM2D: return "Transform2D"
-		TYPE_VECTOR4: return "Vector4"
-		TYPE_VECTOR4I: return "Vector4i"
-		TYPE_PLANE: return "Plane"
-		TYPE_QUATERNION: return "Quaternion"
-		TYPE_AABB: return "AABB"
-		TYPE_BASIS: return "Basis"
-		TYPE_TRANSFORM3D: return "Transform3D"
-		TYPE_PROJECTION: return "Projection"
-		TYPE_COLOR: return "Color"
-		TYPE_STRING_NAME: return "StringName"
-		TYPE_NODE_PATH: return "NodePath"
-		TYPE_RID: return "RID"
-		TYPE_OBJECT: return "Object"
-		TYPE_CALLABLE: return "Callable"
-		TYPE_SIGNAL: return "Signal"
-		TYPE_DICTIONARY: return "Dictionary"
-		TYPE_ARRAY: return "Array"
-		TYPE_PACKED_BYTE_ARRAY: return "PackedByteArray"
-		TYPE_PACKED_INT32_ARRAY: return "PackedInt32Array"
-		TYPE_PACKED_INT64_ARRAY: return "PackedInt64Array"
-		TYPE_PACKED_FLOAT32_ARRAY: return "PackedFloat32Array"
-		TYPE_PACKED_FLOAT64_ARRAY: return "PackedFloat64Array"
-		TYPE_PACKED_STRING_ARRAY: return "PackedStringArray"
-		TYPE_PACKED_VECTOR2_ARRAY: return "PackedVector2Array"
-		TYPE_PACKED_VECTOR3_ARRAY: return "PackedVector3Array"
-		TYPE_PACKED_COLOR_ARRAY: return "PackedColorArray"
-		TYPE_MAX: return "MAX"
-		_: return "Unknown"
