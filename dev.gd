6dc125e64338a4eff3f0909035c7944fc79bb267
SirJoe <me@joehighton.com>
2024-09-22 17:19:08 +0100

    Better Validation Check for error_lookup_function_validation()

--- dev.gd
+++ dev.gd
@@ -5,7 +5,8 @@ func _ready() -> void:
 
 func error_lookup_function_validation():
 	print("Started Function Validation for error_lookup()")
-	for error_code in Toolkit.error_dictionary.keys():
+	var keys = Toolkit.error_dictionary.size() + 1
+	for error_code in keys:
 		var error_info = Toolkit.error_lookup(error_code)
 		print("	Error Code: ", error_code, " | Name: ", error_info.name, " | Description: ", error_info.description)
 	print("Finished Function Validation for error_lookup()")
