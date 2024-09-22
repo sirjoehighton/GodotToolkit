d6a2a28dc57a76718e426369600a53cd968636b5
SirJoe <me@joehighton.com>
2024-09-22 17:17:08 +0100

    Added Start & Stop to error_lookup_function_validation()

--- dev.gd
+++ dev.gd
@@ -4,6 +4,8 @@ func _ready() -> void:
 	error_lookup_function_validation()
 
 func error_lookup_function_validation():
+	print("Started Function Validation for error_lookup()")
 	for error_code in Toolkit.error_dictionary.keys():
-		var error_info = Toolkit.error_dictionary[error_code]
-		print("Error Code: ", error_code, " | Name: ", error_info.name, " | Description: ", error_info.description)
+		var error_info = Toolkit.error_lookup(error_code)
+		print("	Error Code: ", error_code, " | Name: ", error_info.name, " | Description: ", error_info.description)
+	print("Finished Function Validation for error_lookup()")
